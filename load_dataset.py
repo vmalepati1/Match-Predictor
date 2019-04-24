991ba7473ca51dfe3e6255a76c0c46f6be80d599
vmalepati1 <malepativikas@gmail.com>
2019-04-24 18:01:15 -0400

    Switched to returning JSON encoded data. -VM
    Switched to returning JSON encoded data. -VM

--- load_dataset.py
+++ load_dataset.py
@@ -2,6 +2,8 @@ from ping_tba_api import *
 
 def create_dataset(tba_api_key, year):
     events = tba_get_response(tba_api_key, '/events/' + str(year))
-    print(events.content)
+
+    for event in events:
+        event_key = event['key']
 
 create_dataset('1wui0Dih1NifktYrjoXW2hWaMY9XwTfRXaM985Eringd4jeU2raza2nSLXfiALPM', 2016)

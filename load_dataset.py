dee7276d5ba2b9bf07e2ff1ab33471bad332447c
vmalepati1 <malepativikas@gmail.com>
2019-04-24 18:16:25 -0400

    Getting team data. -VM
    Getting team data. -VM

--- load_dataset.py
+++ load_dataset.py
@@ -1,9 +1,19 @@
 from ping_tba_api import *
 
 def create_dataset(tba_api_key, year):
+    x = []
+    Y = []
+    
     events = tba_get_response(tba_api_key, '/events/' + str(year))
 
     for event in events:
         event_key = event['key']
 
-create_dataset('1wui0Dih1NifktYrjoXW2hWaMY9XwTfRXaM985Eringd4jeU2raza2nSLXfiALPM', 2016)
+        for match in tba_get_response(tba_api_key, '/event/' + event_key + '/matches'):
+            red_team_keys = match['alliances']['red']['team_keys']
+            blue_team_keys = match['alliances']['blue']['team_keys']
+
+            
+
+TBA_KEY = '1wui0Dih1NifktYrjoXW2hWaMY9XwTfRXaM985Eringd4jeU2raza2nSLXfiALPM'
+create_dataset(TBA_KEY, 2016)

Attempted to fix moving average issues

create_dataset.py
@@ -80,7 +80,7 @@ class DatasetFactory:
         blue_ma = []
 
         if self.use_scouting_data:
-            data = np.load(self.scouting_input_filepath)
+            data = np.load(self.scouting_input_filepath, allow_pickle=True)
             red_ma = data['red']
             blue_ma = data['blue']
 
@@ -106,7 +106,7 @@ class DatasetFactory:
 
             event_df.set_index('team Number', inplace=True)
 
-        for match in tqdm(self.tba.event_matches(event_key)):
+        for match in self.tba.event_matches(event_key):
             match_number = match['match_number']
 
             # List of team keys
@@ -124,62 +124,67 @@ class DatasetFactory:
             blue_sykes_data = []
 
             # If teams were absent or something else, skip this match
-            try:
-                if self.use_scouting_data:
-                    red_scouting_data = red_ma[match_number]
-                    blue_scouting_data = blue_ma[match_number]
+            #try:
+            if self.use_scouting_data:
+                red_scouting_data = red_ma[match_number]
+                blue_scouting_data = blue_ma[match_number]
 
-                # Team status is nothing but the statistics for the match for the team (ex. how many hatches or cargo placed in Deep Space)
-                for team_key in red_team_keys:
-                    team_status = self.tba.team_status(team_key, event_key)
-                    team_number = self.tba.team(team_key)['team_number']
+            # Team status is nothing but the statistics for the match for the team (ex. how many hatches or cargo placed in Deep Space)
+            for team_key in red_team_keys:
+                team_status = self.tba.team_status(team_key, event_key)
+                team_number = self.tba.team(team_key)['team_number']
 
-                    if self.use_tba_data:
-                        # Number of features has not been set yet
-                        if relevant_sort_order_statistics < 0:
-                            relevant_sort_order_statistics = len(team_status['qual']['sort_order_info'])
+                if self.use_tba_data:
+                    # Number of features has not been set yet
+                    if relevant_sort_order_statistics < 0:
+                        relevant_sort_order_statistics = len(team_status['qual']['sort_order_info'])
 
-                        red_tba_data.append(
-                            team_status['qual']['ranking']['sort_orders'][:relevant_sort_order_statistics])
+                    red_tba_data.append(
+                        team_status['qual']['ranking']['sort_orders'][:relevant_sort_order_statistics])
 
-                    if self.use_sykes_data:
-                        red_sykes_data.append(self.get_team_sykes_data(event_df, team_number))
+                if self.use_sykes_data:
+                    red_sykes_data.append(self.get_team_sykes_data(event_df, team_number))
 
-                for team_key in blue_team_keys:
-                    team_status = self.tba.team_status(team_key, event_key)
-                    team_number = self.tba.team(team_key)['team_number']
+            for team_key in blue_team_keys:
+                team_status = self.tba.team_status(team_key, event_key)
+                team_number = self.tba.team(team_key)['team_number']
 
-                    if self.use_tba_data:
-                        blue_tba_data.append(
-                            team_status['qual']['ranking']['sort_orders'][:relevant_sort_order_statistics])
+                if self.use_tba_data:
+                    blue_tba_data.append(
+                        team_status['qual']['ranking']['sort_orders'][:relevant_sort_order_statistics])
 
-                    if self.use_sykes_data:
-                        blue_sykes_data.append(self.get_team_sykes_data(event_df, team_number))
+                if self.use_sykes_data:
+                    blue_sykes_data.append(self.get_team_sykes_data(event_df, team_number))
 
-                # Perform the indicated statistics operation (sum, average, or median) to combine each team's statistics into one alliance input
-                if self.use_sum:
-                    red_tba_data = np.sum(red_tba_data, axis=0)
-                    red_sykes_data = np.sum(red_sykes_data, axis=0)
-                    blue_tba_data = np.sum(blue_tba_data, axis=0)
-                    blue_sykes_data = np.sum(blue_sykes_data, axis=0)
-                elif self.use_average:
-                    red_tba_data = np.average(red_tba_data, axis=0)
-                    red_sykes_data = np.average(red_sykes_data, axis=0)
-                    blue_tba_data = np.average(blue_tba_data, axis=0)
-                    blue_sykes_data = np.average(blue_sykes_data, axis=0)
-                elif self.use_median:
-                    red_tba_data = np.median(red_tba_data, axis=0)
-                    red_sykes_data = np.median(red_sykes_data, axis=0)
-                    blue_tba_data = np.median(blue_tba_data, axis=0)
-                    blue_sykes_data = np.median(blue_sykes_data, axis=0)
+            # Perform the indicated statistics operation (sum, average, or median) to combine each team's statistics into one alliance input
+            if self.use_sum:
+                red_tba_data = np.sum(red_tba_data, axis=0) if len(red_tba_data) > 0 else []
+                red_sykes_data = np.sum(red_sykes_data, axis=0) if len(red_sykes_data) > 0 else []
+                blue_tba_data = np.sum(blue_tba_data, axis=0) if len(blue_tba_data) > 0 else []
+                blue_sykes_data = np.sum(blue_sykes_data, axis=0) if len(blue_sykes_data) > 0 else []
+            elif self.use_average:
+                red_tba_data = np.average(red_tba_data, axis=0) if len(red_tba_data) > 0 else []
+                red_sykes_data = np.average(red_sykes_data, axis=0) if len(red_sykes_data) > 0 else []
+                blue_tba_data = np.average(blue_tba_data, axis=0) if len(blue_tba_data) > 0 else []
+                blue_sykes_data = np.average(blue_sykes_data, axis=0) if len(blue_sykes_data) > 0 else []
+            elif self.use_median:
+                red_tba_data = np.median(red_tba_data, axis=0) if len(red_tba_data) > 0 else []
+                red_sykes_data = np.median(red_sykes_data, axis=0) if len(red_sykes_data) > 0 else []
+                blue_tba_data = np.median(blue_tba_data, axis=0) if len(blue_tba_data) > 0 else []
+                blue_sykes_data = np.median(blue_sykes_data, axis=0) if len(blue_sykes_data) > 0 else []
 
-                # Flatten red input and blue input arrays to contain all alliance information
-                red_input = np.concatenate((red_tba_data, red_scouting_data, red_sykes_data), axis=None).tolist()
-                blue_input = np.concatenate((blue_tba_data, blue_scouting_data, blue_sykes_data), axis=None).tolist()
-                print(red_input)
-            except:
-                print('Error at event %s match number %d! Skipping...' % (event_short_name, match_number))
-                continue
+            # Flatten red input and blue input arrays to contain all alliance information
+            red_input = np.concatenate((red_tba_data, red_scouting_data, red_sykes_data), axis=None).tolist()
+            blue_input = np.concatenate((blue_tba_data, blue_scouting_data, blue_sykes_data), axis=None).tolist()
+            print(red_scouting_data)
+            print(red_tba_data)
+            print(blue_scouting_data)
+            print(blue_tba_data)
+            print(red_input)
+            print(blue_input)
+            #except:
+                #print('Error at event %s match number %d! Skipping...' % (event_short_name, match_number))
+                #continue
 
             red_score = match['alliances']['red']['score']
             blue_score = match['alliances']['blue']['score']

match_predictor.py
@@ -6,12 +6,12 @@ from sklearn.preprocessing import MinMaxScaler
 class MatchPredictor:
 
     def __init__(self, dataset_filepath):
-        X = np.load(dataset_filepath)['x']
-        y = np.load(dataset_filepath)['y']
+        self.X = np.load(dataset_filepath)['x']
+        self.y = np.load(dataset_filepath)['y']
 
-        self.number_of_features = len(X[0])
+        self.number_of_features = len(self.X[0])
 
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, random_state=0)
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, random_state=0)
 
         self.scaler = MinMaxScaler()
         self.X_train = self.scaler.fit_transform(self.X_train)
@@ -26,5 +26,5 @@ class MatchPredictor:
         pass
 
     # Visualize the model
-    def visualize():
+    def visualize(self):
         pass

scouting_data/moving_average/ma_calculator.py
@@ -59,89 +59,91 @@ class MACalculator:
             self.previous_team_dict[team_number].append([float(i) for i in row[2:]])
 
     def calculate_current_mas(self):
-        two_alliances_g = []
+        previous_match_num = int(self.current_td_rows[0][1])
 
-        previous_match_num = 0
+        red_alliance_data = []
+        blue_alliance_data = []
 
         for match_rec in self.current_td_rows:
             current_match_num = int(match_rec[1])
 
             if current_match_num != previous_match_num:
-                two_alliances_g = []
+                if self.team_combination_metric == 's':
+                    red_alliance_data_metric = np.sum(red_alliance_data, axis=0)
+                    blue_alliance_data_metric = np.sum(blue_alliance_data, axis=0)
+                elif self.team_combination_metric == 'a':
+                    red_alliance_data_metric = np.average(red_alliance_data, axis=0)
+                    blue_alliance_data_metric = np.average(blue_alliance_data, axis=0)
+                elif self.team_combination_metric == 'm':
+                    red_alliance_data_metric = np.median(red_alliance_data, axis=0)
+                    blue_alliance_data_metric = np.median(blue_alliance_data, axis=0)
+                else:
+                    # User does not desire to condense training features
+                    red_alliance_data_metric = [item for sublist in red_alliance_data for item in sublist]
+                    blue_alliance_data_metric = [item for sublist in blue_alliance_data for item in sublist]
 
-            two_alliances_g.append(match_rec)
+                print(red_alliance_data_metric)
+                print(blue_alliance_data_metric)
+
+                self.red_moving_averages.append(red_alliance_data_metric)
+                self.blue_moving_averages.append(blue_alliance_data_metric)
+
+                print('Processed match number %d' % previous_match_num)
+
+                red_alliance_data = []
+                blue_alliance_data = []
 
             previous_match_num = current_match_num
 
-            red_alliance_data = []
-            blue_alliance_data = []
+            team_number = int(match_rec[0])
+            current_team_data = [float(i) for i in match_rec[2:]]
 
-            for team in two_alliances_g:
-                team_number = int(team[0])
-                current_team_data = [float(i) for i in team[2:]]
+            if team_number not in self.current_team_dict:
+                self.current_team_dict[team_number] = []
 
-                if team_number not in self.current_team_dict:
-                    self.current_team_dict[team_number] = []
+            most_recent_cur_data = self.current_team_dict[team_number]
 
-                most_recent_cur_data = self.current_team_dict[team_number]
-
-                num_prev_matches_needed = self.time_period - len(most_recent_cur_data)
-                if num_prev_matches_needed > 0:
-                    # New team in this competition that was not in previous competition
-                    try:
-                        most_recent_prev_data = self.previous_team_dict[team_number][-num_prev_matches_needed:]
+            num_prev_matches_needed = self.time_period - len(most_recent_cur_data)
+            if num_prev_matches_needed > 0:
+                # New team in this competition that was not in previous competition
+                try:
+                    most_recent_prev_data = self.previous_team_dict[team_number][-num_prev_matches_needed:]
+                    if len(most_recent_cur_data) > 0:
                         team_most_recent_average_stats = np.average(most_recent_prev_data + most_recent_cur_data,
-                                                                    axis=0)
-                    except KeyError:
-                        team_known_stats = most_recent_cur_data + [current_team_data]
-                        team_most_recent_average_stats = np.average(team_known_stats, axis=0)
-                else:
-                    team_most_recent_average_stats = np.average(most_recent_cur_data[-self.time_period:], axis=0)
-
-                red_alliance_teams = \
-                    self.tba.match(year=self.year, event=self.event_key, type='qm', number=current_match_num)['alliances'][
-                        'red'][
-                        'team_keys']
-
-                blue_alliance_teams = \
-                    self.tba.match(year=self.year, event=self.event_key, type='qm', number=current_match_num)['alliances'][
-                        'blue'][
-                        'team_keys']
-
-                for team_key in red_alliance_teams:
-                    if team_number == self.tba.team(team_key)['team_number']:
-                        red_alliance_data += team_most_recent_average_stats.tolist()
-
-                for team_key in blue_alliance_teams:
-                    if team_number == self.tba.team(team_key)['team_number']:
-                        blue_alliance_data += team_most_recent_average_stats.tolist()
-
-                self.current_team_dict[team_number].append(current_team_data)
-
-            if self.team_combination_metric == 's':
-                red_alliance_data_metric = np.sum(red_alliance_data, axis=0)
-                blue_alliance_data_metric = np.sum(blue_alliance_data, axis=0)
-            elif self.team_combination_metric == 'a':
-                red_alliance_data_metric = np.average(red_alliance_data, axis=0)
-                blue_alliance_data_metric = np.average(blue_alliance_data, axis=0)
-            elif self.team_combination_metric == 'm':
-                red_alliance_data_metric = np.median(red_alliance_data, axis=0)
-                blue_alliance_data_metric = np.median(blue_alliance_data, axis=0)
+                                                                    axis=1)
+                    else:
+                        team_most_recent_average_stats = np.average(most_recent_prev_data, axis=0)
+                except KeyError or TypeError:
+                    team_known_stats = [current_team_data] + most_recent_cur_data
+                    team_most_recent_average_stats = np.average(team_known_stats, axis=0)
             else:
-                # User does not desire to condense training features
-                red_alliance_data_metric = red_alliance_data
-                blue_alliance_data_metric = blue_alliance_data
+                team_most_recent_average_stats = np.average(most_recent_cur_data[-self.time_period:], axis=0)
 
-            self.red_moving_averages.append(red_alliance_data_metric)
-            self.blue_moving_averages.append(blue_alliance_data_metric)
+            red_alliance_teams = \
+                self.tba.match(year=self.year, event=self.event_key, type='qm', number=current_match_num)['alliances'][
+                    'red'][
+                    'team_keys']
 
-            print('Processing match number %d' % current_match_num)
+            blue_alliance_teams = \
+                self.tba.match(year=self.year, event=self.event_key, type='qm', number=current_match_num)['alliances'][
+                    'blue'][
+                    'team_keys']
+
+            for team_key in red_alliance_teams:
+                if team_number == self.tba.team(team_key)['team_number']:
+                    red_alliance_data.append(team_most_recent_average_stats.tolist())
+
+            for team_key in blue_alliance_teams:
+                if team_number == self.tba.team(team_key)['team_number']:
+                    blue_alliance_data.append(team_most_recent_average_stats.tolist())
+
+            self.current_team_dict[team_number].append(current_team_data)
 
         np.savez(self.output_filepath, red=np.array(self.red_moving_averages),
                  blue=np.array(self.blue_moving_averages))
 
 
-tester = MACalculator('C:\\Users\\malep\\OneDrive\\Documents\\Programming\\Private\\client_secret.json',
+tester = MACalculator('C:\\Users\\Vikas Malepati\\Documents\\Programming\\Private\\client_secret.json',
                       '1wui0Dih1NifktYrjoXW2hWaMY9XwTfRXaM985Eringd4jeU2raza2nSLXfiALPM', 2019, '2019gadal',
                       '1Pgf_6Te2ssrva0vlkLGMnzL7vbWpuBp_JyVZKro43b0', '10qU_UfaTLN7zof8jTD-Op1uPONufrDcSBPyfDHd2mC4',
                       'scouting_input.npz',

scouting_data/moving_average/testing.py
@@ -1,9 +1,12 @@
+import numpy as np
+
 current_team_data = []
 # print(current_team_data)
 
 previous_team_data = []
-previous_team_data += [1, 2, 3, 4]
-previous_team_data += [2, 3, 4, 5]
+previous_team_data.append([1, 2, 3, 4])
+previous_team_data.append([2, 3, 4, 5])
+print(np.average([[[1, 2, 3, 4]] + [[2, 3, 4, 5], [2, 3, 4, 5]]], axis=1))
 
 i = 0
 for i in range(0, 5):
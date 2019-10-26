a20aba776f78a728d5662c39d09fcc2d1baf641a
vmalepati1 <malepativikas@gmail.com>
2019-10-25 22:48:40 -0400

    Split dataset

--- match_predictor.py
+++ match_predictor.py
@@ -1,13 +1,21 @@
 import numpy as np
-
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
 
 # Abstract class for match scores prediction
 class MatchPredictor:
 
     def __init__(self, dataset_filepath):
-        self.X = np.load(dataset_filepath)['x']
-        self.y = np.load(dataset_filepath)['y']
-        self.number_of_features = len(self.X[0])
+        X = np.load(dataset_filepath)['x']
+        y = np.load(dataset_filepath)['y']
+
+        self.number_of_features = len(X[0])
+
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, random_state=0)
+
+        self.scaler = MinMaxScaler()
+        self.X_train = self.scaler.fit_transform(self.X_train)
+        self.X_test = self.scaler.transform(self.X_test)
 
     # Trains the model
     def train(self):
